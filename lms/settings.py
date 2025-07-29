07ee230a8852418727294aa82725fa1db487c34e
Luis Mario Suárez <lmcervantessuarez@gmail.com>
2025-07-29 13:49:54 -0600

    Update navbar, launch button, and email password config
    Removed 'active' class from the home link in the navbar and updated a dropdown item label. Added a shopping cart icon to the launch page's access button. Changed email password configuration in settings.py to use an environment variable for improved security.

--- lms/settings.py
+++ lms/settings.py
@@ -189,7 +189,8 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'lmcervantessuarez@gmail.com'
-EMAIL_HOST_PASSWORD = 'zjsn tvah gruo cpmd'
+# Asegúrate de definir esta variable en tu .env
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 # Configuracion de mensajes
 
