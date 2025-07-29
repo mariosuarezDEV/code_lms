9ca497c1fc4e1d1c9954c2cf7ce01a5374273e71
Luis Mario Suárez <lmcervantessuarez@gmail.com>
2025-07-28 18:48:39 -0600

    Disable debug mode in Django settings
    Set DEBUG to False in lms/settings.py to improve security. This change is necessary for production environments.

--- lms/settings.py
+++ lms/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-!j=m1$8(&s*ud30$x*5gw75malqymx9=!q1d@f6-1c&u!-!d%)"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
