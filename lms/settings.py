547aabe20ae91fd0a604fb7266a91880fc10ac97
Luis Mario Suárez <lmcervantessuarez@gmail.com>
2025-07-28 19:48:03 -0600

    Change default email backend to SMTP
    Updated the default EMAIL_BACKEND from Django's console backend to the SMTP backend for sending real emails. This change enables email functionality in production environments by default.

--- lms/settings.py
+++ lms/settings.py
@@ -181,7 +181,7 @@ ACCOUNT_EMAIL_CONFIRMATION_REDIRECT_URL = "landing"
 ACCOUNT_EMAIL_CONFIRMATION_EXPIRE_DAYS = 3
 
 EMAIL_BACKEND = os.getenv(
-    'EMAIL_BACKEND', 'django.core.mail.backends.console.EmailBackend')
+    'EMAIL_BACKEND', 'django.core.mail.backends.smtp.EmailBackend')
 EMAIL_HOST = os.getenv('EMAIL_HOST', 'smtp.gmail.com')
 EMAIL_PORT = int(os.getenv('EMAIL_PORT', 587))
 EMAIL_USE_TLS = True
